dddce2a328735eebc3b850c6f62f11a3b96f2866
fede martinez <federicoamodil23@gmail.com>
2023-08-03 19:24:16 -0300

    fff

---
+++ js/main.js
@@ -0,0 +1,12 @@
+ let usuaio1 = "Fede";
+ let usuario2 = "Mafi";
+
+  let nombreDeUsuario = prompt ("ingrese su nombre de usuario");
+
+  if (nombreDeUsuario=="Fede"){
+    alert ("bienvenido " + nombreDeUsuario)
+  } else if (nombreDeUsuario=="Mafi") {
+    alert ("bienvenido " + nombreDeUsuario)
+  } else {
+    alert ("Andate impostor")
+  };
